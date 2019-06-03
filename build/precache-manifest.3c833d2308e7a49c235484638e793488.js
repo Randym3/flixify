388eece7c7e466ae6d72c8731cb7cd95ce67aca1
Randym3 <MenesesRandy@gmail.com>
2019-06-03 12:14:33 -0700

    installed github pages

---
+++ build/precache-manifest.3c833d2308e7a49c235484638e793488.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "059d1402da5536eab6814bd0078271e7",
+    "url": "/flixify/index.html"
+  },
+  {
+    "revision": "3758622bd10ddf69e6ec",
+    "url": "/flixify/static/css/2.bc131c05.chunk.css"
+  },
+  {
+    "revision": "38938965470019fb2786",
+    "url": "/flixify/static/css/main.1e6f3150.chunk.css"
+  },
+  {
+    "revision": "3758622bd10ddf69e6ec",
+    "url": "/flixify/static/js/2.bc8494dd.chunk.js"
+  },
+  {
+    "revision": "38938965470019fb2786",
+    "url": "/flixify/static/js/main.a9cb31cf.chunk.js"
+  },
+  {
+    "revision": "fcf3c764c0cc2889f7d6",
+    "url": "/flixify/static/js/runtime~main.6493c70a.js"
+  },
+  {
+    "revision": "2246dc72219bef9c17ed4e5678036e0d",
+    "url": "/flixify/static/media/noImage.2246dc72.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/flixify/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/flixify/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/flixify/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/flixify/static/media/slick.f97e3bbf.svg"
+  }
+]);
